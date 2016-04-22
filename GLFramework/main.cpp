f18f2e2e74ec7d37aa80e5e6e20c80116684bb13
Peter Kennedy <peterkennedy@peters-mbp.attlocal.net>
2016-04-22 12:48:05 -0700

    Initial Commit

---
+++ GLFramework/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GLFramework
+//
+//  Created by Peter Kennedy on 4/22/16.
+//  Copyright (c) 2016 Peter Kennedy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
